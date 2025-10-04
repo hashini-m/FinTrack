c52f4ae19682cb08444302778d12dd663198de68
HashiniM <hashie.mudannayake@gmail.com>
2025-10-04 07:20:56 +0530

    app keys

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,7 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-import { firebaseConfig } from "./config";
+
+import Constants from "expo-constants";
+
+const firebaseConfig = {
+  apiKey: Constants.expoConfig.extra.firebaseApiKey,
+  authDomain: Constants.expoConfig.extra.firebaseAuthDomain,
+  projectId: Constants.expoConfig.extra.firebaseProjectId,
+  storageBucket: Constants.expoConfig.extra.firebaseStorageBucket,
+  messagingSenderId: Constants.expoConfig.extra.firebaseMessagingSenderId,
+  appId: Constants.expoConfig.extra.firebaseAppId,
+};
 
 const app = initializeApp(firebaseConfig);
 
